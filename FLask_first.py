588d164be28cf86bd09b58faf2a842a6b63388a8
Rohan <rohan4102125@gmail.com>
2020-07-13 13:37:30 +1000

    changed address to IP

--- FLask_first.py
+++ FLask_first.py
@@ -29,5 +29,5 @@ def messages():
     loop.run_until_complete(task)
 
 if __name__ == '__main__':
-    app.run('https://rohansbot.azurewebsites.net')
+    app.run('20.36.106.96')
     #app.run('localhost',3978)
